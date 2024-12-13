64d0701ad7d80c40cf35ddfc101c978ce3860d50
Ben Gutzler <bgutzler@gmail.com>
2024-12-13 11:19:28 -0500

    Update C-HAT_TestAnalogInputs.ino
    Add Hall effect sensor

--- C-HAT_TestAnalogInputs.ino
+++ C-HAT_TestAnalogInputs.ino
@@ -6,7 +6,7 @@
  *  to change as you alter the condition.
  *  I find a finger covering and uncovering the IR and light
  *  sensors is good, as is pressing a thumb over the temp sensor.
- *  
+ *  I keep a magnet by the desk to test the Hall effect sensor.
  *  Don't forget to reload the main C-HAT sketch onto the logger
  *  before returning it to service!
  */
@@ -14,6 +14,7 @@
 int A0read; //IR
 int A1read; //temp
 int A2read; //light
+int A3read; //Hall effect
 
 
 void setup() {
@@ -28,11 +29,15 @@ analogRead(A1);
 A1read = analogRead(A1);
 analogRead(A2);
 A2read = analogRead(A2);
+analogRead(A3);
+A3read = analogRead(A3);
 Serial.print(A0read);
 Serial.print(",");
 Serial.print(A1read);
 Serial.print(",");
-Serial.println(A2read);
+Serial.print(A2read);
+Serial.print(",");
+Serial.println(A3read);
 delay(500);
 
 }
